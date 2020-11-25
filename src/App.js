235fac267a06efbdbb98cda40c92bcadc0efc4cd
tycoon <smizidor@gmail.com>
2020-11-25 08:07:01 +0100

    error handling

--- src/App.js
+++ src/App.js
@@ -8,20 +8,44 @@ function App() {
   const APIKEY = '39f9e0098e77a223f0e6430f2535b2e4'
 
   async function fetchData(e) {
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
       e.preventDefault()
-    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${APIKEY}`)
+    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`)
       .then( res => res.json())
       .then(data => data)
+      if(city && country) {
       setWeather({
-        data: apiData
+        data: apiData,
+        city: apiData.city,
+        country: apiData.sys.country,
+        description: apiData.weather[0].description,
+        temperature: apiData.main.temp,
+        error:""
       }
-      )
+      )} else {
+        setWeather({
+          data: '',
+          city: '',
+          country: '',
+          description: '',
+          temperature: '',
+          error:"Please Type A City And Country"
+      }
+      )}
   }
 
   return (
     <div className="App">
       <h3>WEATHER APP</h3>
       <Form getWeather={fetchData} />
+      <Weather
+      city={weather.city}
+      country={weather.country}
+      description={weather.description}
+      temperature={weather.temperature}
+      error={weather.error}
+      />
       {console.log(weather.data)}
     </div>
   );
